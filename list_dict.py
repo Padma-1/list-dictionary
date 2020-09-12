57ef493be8af5b8eae15d9c660bcad1ba12f992f
Padma-1 <65107839+Padma-1@users.noreply.github.com>
2020-09-12 22:28:59 +0530

    Update list_dict.py

--- list_dict.py
+++ list_dict.py
@@ -2,6 +2,9 @@ x = ['a','b','c','d','e']
 y = [1,2,3,4,5]
 z= { }
 for i in range(len(x)):
-           z[y[i]] = x[i]
+           #z[y[i]] = x[i]
+           z=dict(zip(y,x))
+ 
+
 print(z)
 #output-->{1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e'}
